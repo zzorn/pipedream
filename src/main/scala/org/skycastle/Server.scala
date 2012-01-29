144c497606a8d3822e9061e8c5ec97ee2549c5cc
zzorn <zzorn@killy.(none)>
2012-01-29 21:16:28 +0200

    Updated logging to use log4j.

--- src/main/scala/org/skycastle/Server.scala
+++ src/main/scala/org/skycastle/Server.scala
@@ -2,6 +2,7 @@ package org.skycastle
 
 import util.Logging
 import world.Scheduler
+import org.apache.log4j.PropertyConfigurator
 
 /**
  * Starts a new server.
@@ -9,6 +10,9 @@ import world.Scheduler
 object Server extends Logging {
 
   def main(args: Array[ String ]) {
+
+    initializeLogging()
+
     log.info("Starting Skycastle Server")
 
     // TODO: Read config from command line parameters or default location
@@ -20,14 +24,15 @@ object Server extends Logging {
     // TODO: Start listening for connecting clients, and running simulation update code.
 
 
-
-
     // Testing scheduler..
     Scheduler.addRegularTask(interval = 2){duration => println("Tick "+ duration)}
     Scheduler.addRegularTask(1, 2){duration => println("Tock "+duration)}
     Scheduler.addSingleTask(10){println("Bongg!"); Thread.sleep(2235) }
     Scheduler.addVariableTask(3){duration => println("Brick! " + duration); (math.random * 10).floatValue()}
     Scheduler.loop()
+
   }
 
 }
+
+
--- src/main/scala/org/skycastle/util/Logging.scala
+++ src/main/scala/org/skycastle/util/Logging.scala
@@ -1,13 +1,23 @@
 package org.skycastle.util
 
 import org.slf4j.{Logger, LoggerFactory}
+import org.apache.log4j.PropertyConfigurator
 
 /**
  * A mixin that provides various logging methods that delegate to the SLF4J framework.
  */
 trait Logging {
+
   def loggingPath = getClass
 
   val log: Logger = LoggerFactory.getLogger(loggingPath)
+
+  /**
+   * Should be called from entry point / main class one as the application starts up, to configure the logging used.
+   */
+  def initializeLogging() {
+    // Setup log4j logging using config file
+    PropertyConfigurator.configure("log4j.properties")
+  }
 
 }
